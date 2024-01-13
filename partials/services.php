5a606792f64651be2368594d00f50f32b291a071
David <david96.des@gmail.com>
2024-01-12 23:58:30 -0400

    partial de servicios

---
+++ partials/services.php
@@ -0,0 +1,15 @@
+<section id="services">
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-md-6 col-lg-4">
+                Col
+            </div>
+            <div class="col-md-6 col-lg-4">
+                Col
+            </div>
+            <div class="col-md-6 col-lg-4">
+                Col
+            </div>
+        </div>
+    </div>
+</section>
