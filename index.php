5bc7ce53a76689d52c6200e05c6f38d746a29d68
David <david96.des@gmail.com>
2024-01-14 16:45:49 -0400

    Se registro el partial de contact y un sistema de guid para sesiones

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+
+//Graba un token de session para asegurar que es la sesion activa
+ob_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+global $newguid;
+if (function_exists('com_create_guid') === true)
+{
+    $newguid = trim(com_create_guid(), '{}');
+} else {
+    $newguid = sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+}
+
+$_SESSION["link"] = $newguid;
+
+
 include_once "functions/definitions.php";
 include_once "functions/misc.php";
 include_once "functions/translator.php";
@@ -19,6 +37,7 @@ include_once "functions/translator.php";
         <?php include_once "partials/services.php"; ?>
         <?php include_once "partials/schedule.php"; ?>
         <?php include_once "partials/about-process.php"; ?>
+        <?php include_once "partials/contact.php"; ?>
     </main>
     <?php include_once "partials/footer.php"; ?>
     <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
