8c8d3170a9c96d62f187e0aacce74e7659c10cb3
David <david96.des@gmail.com>
2024-01-14 17:19:02 -0400

    Modificaciones de estilo más acorde al estilo general de la pagina

--- partials/schedule.php
+++ partials/schedule.php
@@ -2,58 +2,57 @@
 global $lang_iso;
 include_once "translations/{$lang_iso}.php";
 global $services;
-
 ?>
-
-<section id="schedule" class = "d-none">         
-    <div class = "container-xxl">
-        <div class="box-sm border-radius bg-white p-3">                        
-            <h3 class = "text-center"><?php echo tr("GET_FREE_APPOINTMENT"); ?></h3>                    
-            <form id = "frSchedule">
-                <div class = "row">
-                    <div class = "col-md-2 appfield">
-                        <div class = "border">
-                            <label><?php echo tr("NAME"); ?></label>
-                            <input type = "text" id = "NOMBRE" name = "NOMBRE" value = "" placeholder="<?php echo tr("NAME"); ?>" required>
+<section id="schedule">         
+    <div class="container-xxl">
+        <div class="box border-radius bg-white">                        
+            <h2 class="section-title"><?php echo tr("GET_FREE_APPOINTMENT"); ?></h2>                    
+            <form id="frSchedule">
+                <div class="row g-3">
+                    <div class="col-sm-6 col-lg-4 appfield">
+                        <div class="border">
+                            <label for="NOMBRE"><?php echo tr("NAME"); ?></label>
+                            <input type="text" class="form-control" id="NOMBRE" name="NOMBRE" value="" placeholder="<?php echo tr("NAME"); ?>" required>
                         </div>
                     </div> 
-                    <div class = "col-md-2 appfield">
-                        <div class = "border">
-                            <label><?php echo tr("EMAIL"); ?></label>
-                            <input type = "email" name = "EMAIL" id = "EMAIL" value = "" placeholder="<?php echo tr("EMAIL"); ?>" required>
+                    <div class="col-sm-6 col-lg-4 appfield">
+                        <div class="border">
+                            <label for="EMAIL"><?php echo tr("EMAIL"); ?></label>
+                            <input type="email" class="form-control" name="EMAIL" id="EMAIL" value="" placeholder="<?php echo tr("EMAIL"); ?>" required>
                         </div>
                     </div> 
-                    <div class = "col-md-2 appfield">
-                        <div class = "border">
-                            <label><?php echo tr("CONTACT_NUMBER"); ?></label>
-                            <input type = "text" name = "PHONE" id = "PHONE" value = "" placeholder="<?php echo tr("CONTACT_NUMBER"); ?>" required>
+                    <div class="col-sm-6 col-lg-4 appfield">
+                        <div class="border">
+                            <label for="PHONE"><?php echo tr("CONTACT_NUMBER"); ?></label>
+                            <input type="text" class="form-control" name="PHONE" id="PHONE" value="" placeholder="<?php echo tr("CONTACT_NUMBER"); ?>" required>
                         </div>
                     </div> 
-                    <div class = "col-md-2 appfield">
-                        <div class = "border">
-                        <label><?php echo tr("CAR_MODEL"); ?></label>
-                        <input type = "text" name = "CARMODEL" id = "CARMODEL" value = "" placeholder="<?php echo tr("CAR_MODEL"); ?>" required>
+                    <div class="col-sm-6 col-lg-4 appfield">
+                        <div class="border">
+                            <label for="CARMODEL"><?php echo tr("CAR_MODEL"); ?></label>
+                            <input type="text" class="form-control" name="CARMODEL" id="CARMODEL" value="" placeholder="<?php echo tr("CAR_MODEL"); ?>" required>
                         </div>   
                     </div> 
-                    <div class = "col-md-2 appfield">
-                        <div class = "border">
-                            <label><?php echo tr("SERVICES"); ?></label>
-                            <select id = "SERVICES" name = "SERVICES" id = "SERVICES" value = "" placeholder="<?php echo tr("SERVICES"); ?>" required>
-                            <?php foreach($services as $key => $val): ?>
-                                <option value = "<?php echo $key ?>"><?php echo $val['title']; ?></option>
-                            <?php endforeach; ?> 
+                    <div class="col-sm-6 col-lg-4 appfield">
+                        <div class="border">
+                            <label for="SERVICES"><?php echo tr("SERVICES"); ?></label>
+                            <select id="SERVICES" class="form-select" name="SERVICES" id="SERVICES" value="" placeholder="<?php echo tr("SERVICES"); ?>" required>
+                                <?php foreach($services as $key => $val): ?>
+                                    <option value="<?php echo $key ?>"><?php echo $val['title']; ?></option>
+                                <?php endforeach; ?> 
                             </select>
                         </div>
                     </div> 
-                    <div class = "col-md-2 appfield">
-                        <div class = "border">
-                            <label><?php echo tr("DATE"); ?></label>
-                            <input type = "date" name = "FECHA" id = "FECHA" value = "" placeholder="<?php echo tr("DATE"); ?>" required>
+                    <div class="col-sm-6 col-lg-4 appfield">
+                        <div class="border">
+                            <label for="FECHA"><?php echo tr("DATE"); ?></label>
+                            <input type="date" name="FECHA" class="form-control" id="FECHA" value="" placeholder="<?php echo tr("DATE"); ?>" required>
                         </div>
-                    </div> 
+                    </div>
+                    <div class="col-12" id="mensajes-schedule"></div>
                 </div>
-                <div class = "d-flex pt-5">
-                    <input type = "hidden" id = "TOKEN" value = "<?php global $newguid; echo $newguid; ?>">
+                <div class="d-flex justify-content-center justify-content-md-end">
+                    <input type="hidden" id="TOKEN" value="<?php global $newguid; echo $newguid; ?>">
                     <button id="btAppoiment" class="btn btn-primary float-right"><?php echo tr("GET_APPOIMENT"); ?></button>
                 </div>                 
             </form>                        
