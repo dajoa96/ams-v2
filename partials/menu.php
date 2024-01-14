530a8aacf0f18c13bef9bb7c625a0e8984f7a622
David <david96.des@gmail.com>
2024-01-13 23:57:26 -0400

    Reasignados unos anchors, traducciones de items y eliminacion del nombre idioma para ahorrar espacio sobre todo en español

--- partials/menu.php
+++ partials/menu.php
@@ -15,14 +15,14 @@ global $available_languages;
         </div>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="#services" class="btn btn-sm btn-primary text-start justify-content-start"><i class="fas fa-bars"></i>Services</a>
-                <a href="#about" class="nav-item nav-link">About Us</a>
-                <a href="#contact" class="nav-item nav-link">Contact</a>
-                <a href="#services" class="nav-item nav-link">Schedule a Repair</a>
+                <a href="#services" class="btn btn-sm btn-primary text-start justify-content-start"><i class="fas fa-bars"></i><?php echo tr("SERVICES"); ?></a>
+                <a href="#about-process" class="nav-item nav-link"><?php echo tr("ABOUT_US"); ?></a>
+                <a href="#schedule" class="nav-item nav-link"><?php echo tr("CONTACT"); ?></a>
+                <a href="#services" class="nav-item nav-link"><?php echo tr("SCHEDULE_A_REPAIR"); ?></a>
                 <a href="tel:17869316389" class="nav-item nav-link">(786) 931-6389</a>
                 <?php if (is_array($available_languages) && count($available_languages)): ?>
                 <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Language <img src="assets/img/<?php echo $lang_iso; ?>.png" alt="Selected Language" width="30px" height="20px"></a>
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><img src="assets/img/<?php echo $lang_iso; ?>.png" alt="Selected Language" width="30px" height="20px"></a>
                     <div class="dropdown-menu dropdown-menu-end fade-up m-0" style="left: unset; right: 0;">
                         <?php foreach($available_languages as $key): ?>
                             <?php if($key == $lang_iso): ?>
