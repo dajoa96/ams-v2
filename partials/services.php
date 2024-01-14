8367d462b0a953cab22f7f788dbfe10315f36041
David <david96.des@gmail.com>
2024-01-13 23:04:08 -0400

    Las traducciones se llevaron a sus archivos correspondientes

--- partials/services.php
+++ partials/services.php
@@ -1,101 +1,13 @@
 <?php
-$services = [
-    'en' => [
-        [
-            'title' => 'Collision <br>Repair',
-            'is_new' => true,
-            'price' => '299$',
-            'image' => 'assets/img/services/colission-repair.png',
-            'description' => "Contact us to arrange a brief 15-minute appointment with a skilled technician. During this time, your vehicle will undergo a thorough inspection, and we'll discuss the necessary  work it requires."
-        ],
-        [
-            'title' => 'Engine <br>Diagnostics',
-            'is_new' => false,
-            'price' => '199$',
-            'image' => 'assets/img/services/engine-diagnostics.png',
-            'description' => "Our state-of-the-art facility, we employ cutting-edge diagnostic tools and highly skilled technicians to identify and address engine-related issues with precision."
-        ],
-        [
-            'title' => 'Paintless <br>Dent Repair',
-            'is_new' => true,
-            'price' => '200$',
-            'image' => 'assets/img/services/paintless-dent-repair.png',
-            'description' => "Our skilled technicians specialize in the art of precision dent removal, utilizing advanced techniques to seamlessly erase dents and dings from your vehicle's body."
-        ],
-        [
-            'title' => 'Mechanical <br>Services',
-            'is_new' => true,
-            'price' => '299$',
-            'image' => 'assets/img/services/mechanical-service.png',
-            'description' => "At our state-of-the-art facility, our skilled technicians combine expertise with cutting-edge technology to address a wide spectrum of mechanical issues, ensuring your vehicle's optimal performance."
-        ],
-        [
-            'title' => 'Paint <br>Repair',
-            'is_new' => false,
-            'price' => '599$',
-            'image' => 'assets/img/services/paint-repair.png',
-            'description' => "Our skilled technicians specialize in restoring the beauty of your vehicle's exterior through meticulous paint repairs, addressing everything from minor scratches to more extensive damage."
-        ],
-        [
-            'title' => 'Detailing',
-            'is_new' => true,
-            'price' => '199$',
-            'image' => 'assets/img/services/detailing.png',
-            'description' => "Our skilled detailers are dedicated to elevating the appearance of your car, both inside and out, through meticulous cleaning, polishing, and protection processes."
-        ]
-    ],
-    'es' => [
-        [
-            'title' => 'Reparación <br>de Colisión',
-            'is_new' => true,
-            'price' => '299$',
-            'image' => 'assets/img/services/colission-repair.png',
-            'description' => "Póngase en contacto con nosotros para concertar una breve cita de 15 minutos con un técnico cualificado. Durante este tiempo, su vehículo se someterá a una inspección completa, y vamos a discutir el trabajo necesario que requiere."
-        ],
-        [
-            'title' => 'Diagnostico <br>de Motor',
-            'is_new' => false,
-            'price' => '199$',
-            'image' => 'assets/img/services/engine-diagnostics.png',
-            'description' => "En nuestras instalaciones de última generación, empleamos herramientas de diagnóstico de vanguardia y técnicos altamente cualificados para identificar y resolver con precisión los problemas relacionados con el motor."
-        ],
-        [
-            'title' => 'Reparación <br>de Rayones de Pintura',
-            'is_new' => true,
-            'price' => '200$',
-            'image' => 'assets/img/services/paintless-dent-repair.png',
-            'description' => "Nuestros cualificados técnicos están especializados en el arte de la desabolladura de precisión, utilizando técnicas avanzadas para borrar a la perfección las abolladuras y golpes de la carrocería de su vehículo."
-        ],
-        [
-            'title' => 'Servicio <br> Mecánico',
-            'is_new' => true,
-            'price' => '299$',
-            'image' => 'assets/img/services/mechanical-service.png',
-            'description' => "En nuestras instalaciones de última generación, nuestros técnicos cualificados combinan su experiencia con la tecnología más avanzada para abordar una amplia gama de problemas mecánicos, garantizando el rendimiento óptimo de su vehículo."
-        ],
-        [
-            'title' => 'Reparación <br>de Pintura',
-            'is_new' => false,
-            'price' => '599$',
-            'image' => 'assets/img/services/paint-repair.png',
-            'description' => "Nuestros cualificados técnicos se especializan en restaurar la belleza del exterior de su vehículo mediante meticulosas reparaciones de pintura, abordando desde pequeños arañazos hasta daños más extensos."
-        ],
-        [
-            'title' => 'Detallado',
-            'is_new' => true,
-            'price' => '199$',
-            'image' => 'assets/img/services/detailing.png',
-            'description' => "Nuestros expertos detallistas se dedican a mejorar el aspecto de su coche, tanto por dentro como por fuera, mediante meticulosos procesos de limpieza, pulido y protección."
-        ]
-    ]
-];
 global $lang_iso;
+include_once "translations/{$lang_iso}.php";
+global $services;
 ?>
 
 <section id="services">
     <div class="container-xxl">
-        <div class="row gy-4">
-            <?php foreach($services[$lang_iso] as $key => $val): ?>
+        <div class="row gy-4">            
+            <?php foreach($services as $key => $val): ?>
                 <div class="col-md-6 col-lg-4">
                     <div class="flip-card">
                         <div class="flip-card-inner border-radius">
