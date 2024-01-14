cad39eb98b7f27ea31a12e97ce61a80cbc9d17c2
David <david96.des@gmail.com>
2024-01-13 23:57:54 -0400

    Cambios en el padding superior

--- partials/banner.php
+++ partials/banner.php
@@ -1,5 +1,5 @@
 <section id="banner">
-    <div class="container-fluid">
+    <div class="container-fluid pt-3 pt-lg-0">
         <div class="banner-container border-radius overflow-hidden position-relative">
             <img src="assets/img/banner-bg.jpg" class="img-fluid w-100" alt="Banner Background Image">
             <div class="banner-inner-container">
