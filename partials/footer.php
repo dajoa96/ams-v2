1f4ac16e5d096e0b5bc6b5fa6f69dd6bb39d4beb
David <david96.des@gmail.com>
2024-01-13 22:14:42 -0400

    Seccion de footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,70 @@
 <footer id="footer">
-    
+    <div class="container-xxl">
+        <div class="bg-primary box border-radius">
+            <div class="row gy-4">
+                <div class="col-lg-6 col-ready">
+                    <div class="col-ready-wrapper">
+                        <h2><?php echo tr("READY_TO_START"); ?></h2>
+                        <div class="paragraph">
+                            <p><?php echo tr("READY_TO_START_PARAGRAPH_1"); ?></p>
+                            <p><?php echo tr("READY_TO_START_PARAGRAPH_2"); ?></p>
+                            <p><a href="tel:17869316389">(786) 931-6389</a> - <a href="mailto:automotions305@gmail.com">automotions305@gmail.com</a></p>
+                            <address><a href="https://maps.app.goo.gl/fJ8VKfvHFzDKfqwQA" target="_blank">9605 NW 79TH AVE UNIT 4-5 hialeah gardens, Fl, 33016</a></address>
+                        </div>
+                        <div class="contact-buttons">
+                            <div class="row gy-3">
+                                <div class="col-sm-6">
+                                    <a href="tel:17869316389" class="btn btn-light"><?php echo tr("CALL_US_NOW"); ?></a>
+                                </div>
+                                <div class="col-sm-6">
+                                    <a href="https://maps.app.goo.gl/fJ8VKfvHFzDKfqwQA" class="btn btn-outline-light"><?php echo tr("VISIT_US_TODAY"); ?></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-images">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="ready-item text-center text-md-start">
+                                <div class="ready-item-image">
+                                    <img src="assets/img/footer/our-skills.svg" alt="<?php echo tr("OUR_SKILLS"); ?>" class="img-fluid">
+                                </div>
+                                <h3 class="ready-item-title my-2 my-sm-3"><?php echo tr("OUR_SKILLS"); ?></h3>
+                                <p class="ready-item-description"><?php echo tr("OUR_SKILLS_DESCRIPTION"); ?></p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="ready-item text-center text-md-start">
+                                <div class="ready-item-image">
+                                    <img src="assets/img/footer/our-vision.svg" alt="<?php echo tr("OUR_VISION"); ?>" class="img-fluid">
+                                </div>
+                                <h3 class="ready-item-title my-2 my-sm-3"><?php echo tr("OUR_VISION"); ?></h3>
+                                <p class="ready-item-description"><?php echo tr("OUR_VISION_DESCRIPTION"); ?></p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="ready-item text-center text-md-start">
+                                <div class="ready-item-image">
+                                    <img src="assets/img/footer/our-goal.svg" alt="<?php echo tr("OUR_GOAL"); ?>" class="img-fluid">
+                                </div>
+                                <h3 class="ready-item-title my-2 my-sm-3"><?php echo tr("OUR_GOAL"); ?></h3>
+                                <p class="ready-item-description"><?php echo tr("OUR_GOAL_DESCRIPTION"); ?></p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="ready-item text-center text-md-start">
+                                <div class="ready-item-image">
+                                    <img src="assets/img/footer/our-purpose.svg" alt="<?php echo tr("OUR_PURPOSE"); ?>" class="img-fluid">
+                                </div>
+                                <h3 class="ready-item-title my-2 my-sm-3"><?php echo tr("OUR_PURPOSE"); ?></h3>
+                                <p class="ready-item-description"><?php echo tr("OUR_PURPOSE_DESCRIPTION"); ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p class="text-center mb-3 mt-4"><?php echo tr("DEVELOPED_BY"); ?> <strong><a href="https://paresoftware.com" target="_blank">PareSoftware</a></strong> &copy; 2024</p>
+    </div>
 </footer>
