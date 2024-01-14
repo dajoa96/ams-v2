a8a399fea67f7fefa48f3faa94356576aad2531f
David <david96.des@gmail.com>
2024-01-13 23:24:05 -0400

    Borrado codigo innecesario y se saco el banner de la etiqueta header

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 include_once "functions/definitions.php";
 include_once "functions/misc.php";
 include_once "functions/translator.php";
-// echo $lang_iso;
-// exit;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,15 +13,14 @@ include_once "functions/translator.php";
     <?php include_once "partials/spinner.php"; ?>
     <header>
         <?php include_once "partials/menu.php"; ?>
-        <?php include_once "partials/banner.php"; ?>
     </header>
     <main>
+        <?php include_once "partials/banner.php"; ?>
         <?php include_once "partials/services.php"; ?>
         <?php include_once "partials/schedule.php"; ?>
         <?php include_once "partials/about-process.php"; ?>
     </main>
     <?php include_once "partials/footer.php"; ?>
-    <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
     <?php include_once "partials/requerido.php"; ?>
 </body>
