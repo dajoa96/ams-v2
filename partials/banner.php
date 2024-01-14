0c85e28b8df7bab0b626859fda15bf7934f41538
David <david96.des@gmail.com>
2024-01-14 00:10:55 -0400

    Se agregaron imagenes de fondo

--- partials/banner.php
+++ partials/banner.php
@@ -1,7 +1,7 @@
 <section id="banner">
     <div class="container-fluid pt-3 pt-lg-0">
         <div class="banner-container border-radius overflow-hidden position-relative">
-            <img src="assets/img/banner-bg.jpg" class="img-fluid w-100" alt="Banner Background Image">
+            <img src="assets/img/banner-bg.jpg" class="banner-bg img-fluid w-100 h-100" alt="Banner Background Image">
             <div class="banner-inner-container">
                 <h1><?php echo tr("BANNER_TITLE"); ?></h1>
                 <div class="banner-buttons">
