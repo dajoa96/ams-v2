eee4551eacb9007f290d4318c3c9c7a63a4bba8c
David <david96.des@gmail.com>
2024-01-15 00:07:36 -0400

    Arregladas las traducciones y se modifico una

--- partials/about-process.php
+++ partials/about-process.php
@@ -10,7 +10,7 @@
                     <div class="box box-sm border-radius bg-light">
                         <div class="row justify-content-center gy-3">
                             <div class="col-12 ">
-                                <p class="text-center m-0"><?php //echo tr("ABOUT_US_FIND_CHANNEL"); ?> Find the channel of your preference</p>
+                                <p class="text-center m-0"><?php echo tr("ABOUT_US_FIND_CHANNEL"); ?></p>
                             </div>
                             <div class="col-sm-6 col-md-4 col-lg-3">
                                 <a class="btn btn-light bg-white text-center w-100 btn-facebook" href="#">
@@ -38,33 +38,33 @@
             </div>
             <div class="item our-process">
                 <div class="border-radius box bg-white">
-                    <h2><?php //echo tr("ABOUT_US"); ?>Our Process</h2>
+                    <h2><?php echo tr("ABOUT_US"); ?></h2>
                     <div class="row justify-content-center gy-3">
                         <div class="col-sm-6 col-md-4">
                             <div class="process-item">
                                 <div class="process-image-container d-flex justify-content-center align-items-center">
-                                    <img src="assets/img/our-process/schedule-appointment.png" alt="<?php //echo tr("SCEDULE_APPOINTMENT"); ?>Scedule Appointment" class="img-fluid process-image" style="width: max-content!important">
+                                    <img src="assets/img/our-process/schedule-appointment.png" alt="<?php echo tr("SCEDULE_APPOINTMENT"); ?>" class="img-fluid process-image" style="width: max-content!important">
                                 </div>
-                                <h3 class="process-title text-center"><?php //echo tr("SCEDULE_APPOINTMENT"); ?>Scedule Appointment</h3>
-                                <p class="process-description text-center"><?php //echo tr("SCEDULE_APPOINTMENT_DESCRIPTION"); ?>Contact us to arrange a brief 15-minute appointment with a skilled technician. During this time, your vehicle will undergo a thorough inspection, and we'll discuss the necessary  work it requires.</p>
+                                <h3 class="process-title text-center"><?php echo tr("SCEDULE_APPOINTMENT"); ?></h3>
+                                <p class="process-description text-center"><?php echo tr("SCEDULE_APPOINTMENT_DESCRIPTION"); ?></p>
                             </div>
                         </div>
                         <div class="col-sm-6 col-md-4">
                             <div class="process-item">
                                 <div class="process-image-container d-flex justify-content-center align-items-center">
-                                    <img src="assets/img/our-process/get-your-car-fixed.png" alt="<?php //echo tr("GET_YOUR_CAR_FIXED"); ?>Get your car Fixed" class="img-fluid process-image" style="width: max-content!important">
+                                    <img src="assets/img/our-process/get-your-car-fixed.png" alt="<?php echo tr("GET_YOUR_CAR_FIXED"); ?>" class="img-fluid process-image" style="width: max-content!important">
                                 </div>
-                                <h3 class="process-title text-center"><?php //echo tr("GET_YOUR_CAR_FIXED"); ?>Get your car Fixed</h3>
-                                <p class="process-description text-center"><?php //echo tr("GET_YOUR_CAR_FIXED_DESCRIPTION"); ?>Trust us for end-to-end care - from parts and repairs to insurance coordination, rentals, and timely updates throughout the repair process.</p>
+                                <h3 class="process-title text-center"><?php echo tr("GET_YOUR_CAR_FIXED"); ?></h3>
+                                <p class="process-description text-center"><?php echo tr("GET_YOUR_CAR_FIXED_DESCRIPTION"); ?></p>
                             </div>
                         </div>
                         <div class="col-sm-6 col-md-4">
                             <div class="process-item">
                                 <div class="process-image-container d-flex justify-content-center align-items-center">
-                                    <img src="assets/img/our-process/vehicle-ready.png" alt="<?php //echo tr("VEHICLE_READY"); ?>Vehicle Ready" class="img-fluid process-image" style="width: max-content!important">
+                                    <img src="assets/img/our-process/vehicle-ready.png" alt="<?php echo tr("VEHICLE_READY"); ?>" class="img-fluid process-image" style="width: max-content!important">
                                 </div>
-                                <h3 class="process-title text-center"><?php //echo tr("VEHICLE_READY"); ?>Vehicle Ready</h3>
-                                <p class="process-description text-center"><?php //echo tr("VEHICLE_READY_DESCRIPTION"); ?>Trust us for all your auto care needs - from repairs and parts to insurance coordination, rentals, and timely updates.</p>
+                                <h3 class="process-title text-center"><?php echo tr("VEHICLE_READY"); ?></h3>
+                                <p class="process-description text-center"><?php echo tr("VEHICLE_READY_DESCRIPTION"); ?></p>
                             </div>
                         </div>
                     </div>
