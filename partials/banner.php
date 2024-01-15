7e4a671bec7c4e7e5fa58bf714e3411b093e6322
David <david96.des@gmail.com>
2024-01-15 17:52:41 -0400

    Se cambio un anchor

--- partials/banner.php
+++ partials/banner.php
@@ -6,7 +6,7 @@
                 <h1><?php echo tr("BANNER_TITLE"); ?></h1>
                 <div class="banner-buttons">
                     <a href="#services" class="btn btn-primary btn-banner"><?php echo tr("OUR_SERVICES"); ?></a>
-                    <a href="#schedule" class="btn btn-outline-light btn-banner"><?php echo tr("CONTACT_US"); ?></a>
+                    <a href="#contact" class="btn btn-outline-light btn-banner"><?php echo tr("CONTACT_US"); ?></a>
                 </div>
             </div>
         </div>
